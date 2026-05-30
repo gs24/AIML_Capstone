2aa5f9ec9f4204b286ce474d8a8a3d9ed7c6153c
gaurav srivastava <g.sri2483@gmail.com>
2026-05-30 15:06:25 +0000

    adding test and train split to preprocess and train files

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 TARGET = "Engine Condition"
 
@@ -11,12 +12,31 @@ FEATURES = [
     "lub oil temp",
     "Coolant temp"
 ]
+def test_train_split(data_df):
+    validate_columns(data_df)
 
+    X = data_df[FEATURES]
+    y = data_df[TARGET]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y   # important for classification
+    )
+
+    train_df = pd.concat([X_train, y_train], axis=1)
+    test_df = pd.concat([X_test, y_test], axis=1)
+    return train_df, test_df
 
 def split_features_target(df):
     X = df[FEATURES]
     y = df[TARGET]
+
+    
     return X, y
+
+
 
 
 def validate_columns(df):
